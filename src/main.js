5016447cff574bde22697c987b5160ea209b83b2
vrlnx <vrlinux@pm.me>
2021-03-12 09:09:27 +0000

    Separate secrets and config for better bot managing

--- src/main.js
+++ src/main.js
@@ -2,7 +2,7 @@
 // This project is lead by vrl#8298
 
 const { TOKEN } = require("../handlers/secrets");
-const { prefix } = require("../handlers/secrets");
+const { prefix } = require("../handlers/config");
 const fs = require("fs");
 const { Client, Message } = require("discord.js");
 const client = new Client();
